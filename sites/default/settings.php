b037f1c3c742f0e5cfc8d004e60326b6cd7dabdc
Jayeson Kiyabu <jkiyabu@comast.net>
2017-05-17 14:42:17 -0700

    fix welcome module

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'community_center',
+      'username' => 'ccenter',
+      'password' => 'ccenter',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = '-sZLE5GQ0aT1I0mSYJbIB9uaaPsoL_zlTJ5AZ_KNw0Q';
 
 /**
  * Base URL (optional).
